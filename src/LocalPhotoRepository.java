fb3f421888bd5ca6a3eebef2ad0705a3249888eb
Andy Rivas <andyrivasv@gmail.com>
2014-09-18 14:15:56 -0400

    Remove unused imports

--- src/LocalPhotoRepository.java
+++ src/LocalPhotoRepository.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class LocalPhotoRepository implements PhotoRepository
 {
