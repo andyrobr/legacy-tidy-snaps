ab311400f4c8b75f135f19b8725c5e43b609e66e
Andy Rivas <andyrivasv@gmail.com>
2014-09-19 18:33:42 -0400

    Change application window title

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Tidy Snaps");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
