91f47566084be43552f44a508d45467d41c3d5d4
Faraz Khan <fkhan0485@gmail.com>
2014-09-22 14:36:56 -0400

    new title, button, background:
       -Added new background, new title, buttons

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
         primaryStage.setTitle("Tidy Snaps");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
 
