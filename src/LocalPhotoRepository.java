0ad85bb8b5862c1fe43b346c185509458c55c461
Andy Rivas <andyrivasv@gmail.com>
2014-09-25 19:13:44 -0400

    Remove unused import

--- src/LocalPhotoRepository.java
+++ src/LocalPhotoRepository.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import java.io.IOException;
-import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
