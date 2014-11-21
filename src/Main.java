8b03aed0ff6605c3c788bfd44fab86727fabfb4d
Faraz Khan <fkhan0485@gmail.com>
2014-11-21 13:17:27 -0500

    Changes made to tidy snaps: 	-Redesigned app look,app.fxml and dialogue.fxml, progress meter 	  change and components in grid.i 	  -css style buttons (browse and start)with fx id 	  -dialogue page has two return areas along with label for pixels, 	    resolution and date with fx id

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,8 +13,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
         primaryStage.setTitle("Tidy Snaps");
+//        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+//        Scene scene = new Scene(root,
+//                visualBounds.getWidth(), visualBounds.getHeight());
+//        primaryStage.setScene(scene);
         primaryStage.setScene(new Scene(root, 1280, 800));
+
         primaryStage.show();
+
     }
 
 
